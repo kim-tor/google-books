9b53e0f4688bce67cda91fa904d5e3d5c03e34c6
Kim Torres <kimberley.5118@gmail.com>
2021-02-05 15:59:55 -0500

    created components that I will use

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron"
+    >
+      <div className="jumbotron">
+        <h1 className="display-4 text-center myText">Google Books Search</h1>
+        <br></br>
+        <h2 className="text-center myText">Search for and save books of interest. Built with React.</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
