84c61c1be873431fbcb6e27016f7f3446689fe9c
Kim Torres <kimberley.5118@gmail.com>
2021-09-20 10:53:36 -0400

    updated book model

--- models/book.js
+++ models/book.js
@@ -3,11 +3,13 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
     title: {type: String, required: true},
-    author: {type:String, required: true},
+    subtitle: { type: String },
+    author: {type:[String], required: true},
     description: {type:String, required: true},
     image: {type:String, unique: true, dropDups: true},
-    link: {type: String, required: true}
+    link: {type: String, required: true},
+    googleId: { type: String, required: true, unique: true }
 })
 
-const googleBooks = mongoose.model("googleBooks", bookSchema);
-module.exports = googleBooks;
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
