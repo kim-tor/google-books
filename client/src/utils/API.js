75b42c9862138b6e196b865181184f226f4d5c1e
Kim Torres <kimberley.5118@gmail.com>
2021-09-20 20:31:39 -0400

    working back towards my original implementation , attempting to fix save functionality

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,11 +7,11 @@ export default {
   },
   // Deletes the book with the given id
   deleteBook: function (id) {
-    return axios.delete("/api/books/" + id).then(result => result.data);
+    return axios.delete("/api/books/" + id);
   },
   // Saves a book to the database
   saveBook: function (bookData) {
-    return axios.post("/api/books", bookData).then(result => result.data);
+    return axios.post("/api/books", bookData);
   },
   // Get the saved a books from the database
   savedBooks: function () {
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,8 @@
 // const path = require("path");
 const router = require("express").Router();
 const bookRoutes = require("./books");
-// const googleRoutes = require("./google")
+
 
 // book routes
 router.use("/books", bookRoutes);
-
-// google bookRoutes
-// router.use("/google", googleRoutes);
-
-//for anything else render the html page
-// router.use(function(req,res) {
-//     res.sendFile(path.join(__dirname,"../../client/build/index.html"))
-// })
 module.exports = router;
